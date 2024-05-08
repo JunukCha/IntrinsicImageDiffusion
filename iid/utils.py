77c3c086f36415a269fea0a5d008e48d57814b96
Junuk Cha <jucha@unist.ac.kr>
2024-05-08 16:29:36 -0700

    Update utils.py

--- iid/utils.py
+++ iid/utils.py
@@ -47,6 +47,7 @@ def readPNG(filename):
     if not filename:
         raise ValueError("Empty filename")
     image = (np.asarray(Image.open(filename).convert("RGB")) / 255.0).astype(np.float32)
+    print(sum(image))
     return image
 
 
