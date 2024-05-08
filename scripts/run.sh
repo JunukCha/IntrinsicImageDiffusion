ecb6a591589066f34e7cc974987c95fd25187fe1
Junuk Cha <jucha@unist.ac.kr>
2024-05-08 16:13:20 -0700

    run

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 read -p "folder_name: (e.g. custom): " folder_name
 read -p "image_name: (e.g. 000.jpg): " image_name
 
-# python -m iid.geometry_prediction logger=console data.input_path=data/$folder_name/im/$image_name output.folder=data/$name
-# python -m iid.material_diffusion logger=console data.input_path=data/$folder_name/im/$image_name output.folder=data/$name
+python -m iid.geometry_prediction logger=console data.input_path=data/$folder_name/im/$image_name output.folder=data/$name
+python -m iid.material_diffusion logger=console data.input_path=data/$folder_name/im/$image_name output.folder=data/$name
 python -m iid.lighting_optimization logger=console data.dataset_cfg.root=data/$folder_name callbacks.file_copy.dst=data/$folder_name/lighting/0.ckpt
 python -m iid.test logger=console data.dataset_cfg.root=data/$folder_name ckpt_path=data/$folder_name/lighting/0.ckpt
